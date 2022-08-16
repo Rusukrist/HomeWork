3a315a797fa4c676a69eecc6bba0128b9efbe68e
RusuKrist <rusukrist30@yandex.ru>
2022-08-16 17:48:00 +0500

    Задание 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,3 +5,7 @@
 918 -> 1
 */
 
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Второй цифрой числа является -> "+stringNumber[1]);
