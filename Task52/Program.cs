ac2ab38a648861e542b40037f0e6592800ec5fe0
RusuKrist <rusukrist30@yandex.ru>
2022-08-27 22:12:14 +0500

    Задача 52

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -19,7 +19,7 @@ int[,] GetFillArray(int m, int n)
 
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
-            result[i, j] = new Random().Next(-100, 100);
+            result[i, j] = new Random().Next(10);
     return result;
 }
 
