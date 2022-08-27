89e7f9635079208f45d97b8b3b49aed108ad9d15
RusuKrist <rusukrist30@yandex.ru>
2022-08-27 21:06:49 +0500

    Задача 47

---
+++ Task47/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            result[i, j] = new Random().Next(-100, 100);
+    return result;
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] myArray = GetArray(3, 4, 0, 100);
+PrintArray(myArray);
